975cccaa62ce4ae6f2ead398554a4d33d8c00ed4
dohminion <grego@mindless.com>
2019-02-08 08:32:44 -0800

    Update Test-InternalTimeSync.ps1
    Fixed logic on alert cleared messages.  Added a command to attempt to repair the time drift

--- Test-InternalTimeSync.ps1
+++ Test-InternalTimeSync.ps1
@@ -16,8 +16,8 @@ function Test-ADInternalTimeSync {
    
     .NOTES
     Authors: Mike Kanakos, Greg Onstot
-    Version: 0.7.1
-    Version Date: 1/07/2019
+    Version: 0.8
+    Version Date: 2/08/2019
     
     Event Source 'PSMonitor' will be created
 
@@ -27,6 +27,7 @@ function Test-ADInternalTimeSync {
     17032 - Testing individual systems
     17033 - End of test
     17034 - Alert Email Sent
+    17035 - Automated Repair Attempted
     #>
 
     Begin {
@@ -63,28 +64,32 @@ function Test-ADInternalTimeSync {
                     $emailOutput = "$server - Offset:  $result - Time:$Remotetime  - ReferenceTime: $Referencetime `r`n "
                     Write-Verbose "ALERT - Time drift above maximum allowed threshold on - $server - $emailOutput"
                     Write-eventlog -logname "Application" -Source "PSMonitor" -EventID 17030 -EntryType Warning -message "FAILURE time drift above maximum allowed on $emailOutput `r`n " -category "17030"
-                    $global:CurrentFailure = $true
+                    #attempt to automatically fix the issue
+                    Invoke-Command -ComputerName $server -ScriptBlock { 'w32tm /resync' }
+                    Write-eventlog -logname "Application" -Source "PSMonitor" -EventID 17035 -EntryType Information -message "Remediation script repair was attempted `r`n " -category "17035"
+                    CurrentFailure = $true
                     Send-Mail $emailOutput
                     Write-Verbose "Sending Slack Alert"
                     New-SlackPost "Alert - Time drift above max threashold - $emailOutput"
                 }#end if
+                If (!$CurrentFailure){
+                    Write-Verbose "No Issues found in this run"
+                    $InError = Get-EventLog application -After (Get-Date).AddHours(-24) | where {($_.InstanceID -Match "17030")} 
+                    $errtext = $InError |out-string
+                    If ($errtext -like "*$server*") {
+                        Write-Verbose "Previous Errors Seen"
+                        #Previous run had an alert
+                        #No errors foun during this test so send email that the previous error(s) have cleared
+                        Send-AlertCleared
+                        Write-Verbose "Sending Slack Message - Alert Cleared"
+                        New-SlackPost "The previous alert, for AD Internal Time Sync, has cleared."
+                        #Write-Output $InError
+                    }#End if
+                }#End if
             }#End Foreach
          }#End Process
     End{
         Write-eventlog -logname "Application" -Source "PSMonitor" -EventID 17033 -EntryType Information -message "END of Internal Time Sync Test Cycle ." -category "17033"
-        If (!$CurrentFailure){
-            Write-Verbose "No Issues found in this run"
-            $InError = Get-EventLog application -After (Get-Date).AddHours(-24) | where {($_.InstanceID -Match "17030")} 
-            If ($InError) {
-                Write-Verbose "Previous Errors Seen"
-                #Previous run had an alert
-                #No errors foun during this test so send email that the previous error(s) have cleared
-                Send-AlertCleared
-                Write-Verbose "Sending Slack Message - Alert Cleared"
-                New-SlackPost "The previous alert, for AD Internal Time Sync, has cleared."
-                #Write-Output $InError
-            }#End if
-        }#End if
     }#End End
 }#End Function
 
