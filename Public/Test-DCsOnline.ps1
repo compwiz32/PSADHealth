abd3a212d87376a9d231cd89224b8a4ab01dfb1e
Stephen Valdinger <stephen@chocolatey.io>
2019-02-22 16:25:00 -0600

    Add proper function stuff and mute calls.

--- Public/Test-DCsOnline.ps1
+++ Public/Test-DCsOnline.ps1
@@ -1,28 +1,39 @@
 # Test-DCsOnline.ps1
+Function Test-DCsOnline {
 
-$SMTPServer = 'smtp.bigfirm.biz'
-$MailSender = "AD Health Check Monitor <ADHealthCheck@bigfirm.biz>"
-$MailTo = "michael_kanakos@bigfirm.biz"
-$DClist = (get-adgroupmember "Domain Controllers").name
+    [cmdletbinding()]
+    Param()
 
-Import-Module ActiveDirectory
+    Begin {}
+    
+    Process {
 
-ForEach ($server in $DClist){
+        $SMTPServer = 'smtp.bigfirm.biz'
+        $MailSender = "AD Health Check Monitor <ADHealthCheck@bigfirm.biz>"
+        $MailTo = "michael_kanakos@bigfirm.biz"
+        $DClist = (get-adgroupmember "Domain Controllers").name
 
-    if  ((!(Test-Connection -ComputerName $Server -quiet -count 1)))
-     {
-         $Subject = "Server $Server is offline"
-         $EmailBody = @"
-  
-  
- Server named <font color="Red"><b> $Server </b></font> is offline!
- Time of Event: <font color="Red"><b> $((get-date))</b></font><br/>
- <br/>
- THIS EMAIL WAS AUTO-GENERATED. PLEASE DO NOT REPLY TO THIS EMAIL.
+        Import-Module ActiveDirectory
+
+        ForEach ($server in $DClist){
+
+            if  ((!(Test-Connection -ComputerName $Server -quiet -count 1)))
+            {
+                $Subject = "Server $Server is offline"
+                $EmailBody = @"
+        
+        
+        Server named <font color="Red"><b> $Server </b></font> is offline!
+        Time of Event: <font color="Red"><b> $((get-date))</b></font><br/>
+        <br/>
+        THIS EMAIL WAS AUTO-GENERATED. PLEASE DO NOT REPLY TO THIS EMAIL.
 "@
 
-    Send-MailMessage -To $MailTo -From $MailSender -SmtpServer $SMTPServer 
-    -Subject $Subject -Body $EmailBody -BodyAsHtml
+            Send-MailMessage -To $MailTo -From $MailSender -SmtpServer $SMTPServer 
+            -Subject $Subject -Body $EmailBody -BodyAsHtml
 
-    } #End if
-  }#End Foreach
+            } #End if
+        }#End Foreach
+}
+    End {}
+}
