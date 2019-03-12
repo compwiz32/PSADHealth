eca6a23205a5b64b492134c71b089929d7801b29
Stephen Valdinger <steviecoaster@users.noreply.github.com>
2019-03-11 23:26:49 -0400

    move export from psm1 to psd1 (#42)

--- PSADHealth.psm1
+++ PSADHealth.psm1
@@ -3,13 +3,3 @@ $Public = Get-ChildItem -Path $PSScriptRoot\Public\*.ps1
 $Public | ForEach-Object {
     . $_.FullName
 }
-
-
-$configFile = Get-Content $PSScriptRoot\Private\config.json
-
-If($configFile -contains "Default")
-{
-    Write-Warning -Message "Module loaded with default configuration."
-    Write-Warning -Message "Please run Set-PSConfig to configure module for your environment"
-}
-
