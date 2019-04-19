1cf7420c2a814afe2fa6b78501e994cbe76d9803
dohminion <grego@mindless.com>
2019-04-18 18:34:52 -0700

    Update Get-ADLastBackupDate.ps1
    Replaced script old script with the newer one.

--- Public/Get-ADLastBackupDate.ps1
+++ Public/Get-ADLastBackupDate.ps1
@@ -1,93 +1,118 @@
-# Get-ADLastBackupDate.ps1
-
 function Get-ADLastBackupDate {
     [CmdletBinding()]
     Param()
-
-    
     <#
     .SYNOPSIS
-        Check AD Last Backup Date
+    Check AD Last Backup Date
     
     .DESCRIPTION
-        This script Checks AD for the last backup date
+    This script Checks AD for the last backup date
 
+    .EXAMPLE
+    Run as a scheduled task.  Use Event Log consolidation tools to pull and alert on issues found.
 
+    .EXAMPLE
+    Run in verbose mode if you want on-screen feedback for testing
+   
     .NOTES
-        Authors: Mike Kanakos, Greg Onstot
-        Version: 0.6
-        Version Date: 11/19/2018
+    Authors: Mike Kanakos, Greg Onstot
+    Version: 0.6.1
+    Version Date: 04/18/2019
+    
+    Event Source 'PSMonitor' will be created
+
+    EventID Definition:
+    17050 - Failure
+    17051 - Beginning of test
+    17052 - Successful Test Result
+    17053 - End of test
+    17054 - Alert Email Sent
     #>
 
     Begin {
         Import-Module activedirectory
+
         $null = Get-ADConfig
+
+        $SupportArticle = $Configuration.SupportArticle
+
+        if (![System.Diagnostics.EventLog]::SourceExists("PSMonitor")) {
+            write-verbose "Adding Event Source."
+            New-EventLog -LogName Application -Source "PSMonitor"
+        }#end if
+
+        Write-eventlog -logname "Application" -Source "PSMonitor" -EventID 17051 -EntryType Information -message "START of AD Backup Check ." -category "17051"
         
-    }
-
-
-    Process {
         $Domain = (Get-ADDomain).DNSRoot
-        $Regex =  '\d\d\d\d-\d\d-\d\d'
+        $Regex = '\d\d\d\d-\d\d-\d\d'
         $CurrentDate = Get-Date
         $MaxDaysSinceBackup = $Configuration.MaxDaysSinceBackup
-        #$MaxDaysSinceBackup = '1'
-    
-    
+        
+    }#End Begin
 
+    Process {
         #get the date of last backup from repadmin command using regex
-        $RepOutput = repadmin /showbackup $Domain
-        $LastBackup = @($RepOutput -split '\r?\n' -match $Regex)[0]
-
+        $LastBackup = (repadmin /showbackup $Domain | Select-String $Regex |ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } )[0]
         #Compare the last backup date to today's date
-        $Result = (NEW-TIMESPAN -Start $LastBackup -End $CurrentDate).Days
-            
+        $Result = (New-TimeSpan -Start $LastBackup -End $CurrentDate).Days
+        
+        Write-Verbose "Last Active Directory backup occurred on $LastBackup! $Result days is less than the alert criteria of $MaxDaysSinceBackup day."
+        
+        Write-eventlog -logname "Application" -Source "PSMonitor" -EventID 17052 -EntryType Information -message "SUCCESS - Last Active Directory backup occurred on $LastBackup! $Result days is less than the alert criteria of $MaxDaysSinceBackup day." -category "17052"
+        
         #Test if result is greater than max allowed days without backup
-        If ($null -eq $Result) {
-            $Subject = "Active Directory has never been backed up"
-            $EmailBody = @"
+        If ($Result -gt $MaxDaysSinceBackup) {
             
-        
-            A backup of Active Directory has never been completed. Please perform a backup of AD as soon possible!
-            <br/>
-            THIS EMAIL WAS AUTO-GENERATED. PLEASE DO NOT REPLY TO THIS EMAIL.
-"@
-        } #end if
+            Write-Verbose "Last Active Directory backup occurred on $LastBackup! $Result days is higher than the alert criteria of $MaxDaysSinceBackup day."
             
-        elseif ($Result -gt $MaxDaysSinceBackup) {
-          
-            $Subject = "Last Active Directory backup occurred on $LastBackup!"
-            $EmailBody = @"
+            $emailOutput = "Last Active Directory backup occurred on $LastBackup! $Result days is higher than the alert criteria of $MaxDaysSinceBackup day."
             
-        
-            The last time Active Directory was backed up was on <font color="Red"><b> $LastBackup </b></font> 
-            which was <font color="Red"><b> $Result</b></font> days ago.
+            Write-eventlog -logname "Application" -Source "PSMonitor" -EventID 17050 -EntryType Warning -message "ALERT - AD Backup is not current.  $emailOutput" -category "17050"
             
-            You asked to be alerted when backups are not completed for more that $MaxDaysSinceBackup days!
-            Time of Event: <font color="Red"><b> $(Get-Date) </b></font><br/>
-            <br/>
-            THIS EMAIL WAS AUTO-GENERATED. PLEASE DO NOT REPLY TO THIS EMAIL.
-"@
+            $global:CurrentFailure = $true
 
             $mailParams = @{
                 To = $Configuration.MailTo
                 From = $Configuration.MailFrom
                 SmtpServer = $Configuration.SmtpServer
-                Subject = $Subject
-                Body = $EmailBody
+                Subject = "AD Backup Check Alert! Backup is $Result days old"
+                Body = $emailOutput
                 BodyAsHtml = $true
-            } # end Mailparams
+          }
 
-            Send-MailMessage @mailParams
+          Send-MailMessage @mailParams
 
+        }#End if
+    
+    }#End Process
+    
+    End {
+        
+        Write-eventlog -logname "Application" -Source "PSMonitor" -EventID 17053 -EntryType Information -message "END of AD Backup Check ." -category "17053"
+        
+        If (!$CurrentFailure){
+            Write-Verbose "No Issues found in this run"
+            $InError = Get-EventLog application -After (Get-Date).AddHours(-24) | where {($_.InstanceID -Match "17050")} 
+            
+            If ($InError) {
+                Write-Verbose "Previous Errors Seen"
+                #Previous run had an alert
+                #No errors foun during this test so send email that the previous error(s) have cleared
+                $alertclearedParams = @{
+                    To = $Configuration.MailTo
+                    From = $Configuration.MailFrom
+                    SmtpServer = $Configuration.SmtpServer
+                    Subject = "AD Internal Time Sync - Alert Cleared!"
+                    Body = "The previous Internal AD Time Sync alert has now cleared."
+                    BodyAsHtml = $true
+              }
+    
+              Send-MailMessage @alertclearedParams
+                #Write-Output $InError
+            }#End if
+        
+        }#End if
 
-        #  Send-MailMessage -To $MailTo -From $MailSender -SmtpServer $SMTPServer 
-        # -Subject $Subject -Body $EmailBody -BodyAsHtml
+    }#End End
 
-        } #End elseif
-
-    } #end process
-
-    End { }
-
-} #end function
+}#End Function
