52a5ecf4789fa5b18a3e824da19b861bccfc9971
Thomas Duemesnil <tduemesnil@barnet-europe.com>
2019-12-06 11:00:22 +0100

    Cim Request is now done in one call

--- PSADHealth/Public/Test-ADServices.ps1
+++ PSADHealth/Public/Test-ADServices.ps1
@@ -5,14 +5,14 @@ function Test-ADServices {
     <#
     .SYNOPSIS
     Monitor AD Domain Controller Services
-    
+
     .DESCRIPTION
     This function is used to Monitor AD Domain Controller services and send alerts if any identified services are stopped
 
     .EXAMPLE
-    Run as a scheduled task on a tool server to remotely monitor service status on all DCs in a specified domain.  
+    Run as a scheduled task on a tool server to remotely monitor service status on all DCs in a specified domain.
 
-   
+
     .NOTES
     Authors: Mike Kanakos, Greg Onstot
     Version: 0.0.5
@@ -23,11 +23,8 @@ function Test-ADServices {
         Import-Module ActiveDirectory
         #Creates a global $configuration variable
         $null = Get-ADConfig
-    }
-
-    process {
-        $DClist = (get-adgroupmember "Domain Controllers").name
-        $collection = @('ADWS',
+        $DClist = (Get-ADGroupMember "Domain Controllers").name
+        $Collection = @('ADWS',
             'DHCPServer',
             'DNS',
             'DFS',
@@ -42,36 +39,35 @@ function Test-ADServices {
             'RPCSS',
             'SAMSS',
             'W32Time')
+        $ServiceFilter = ($Collection | ForEach-Object { "name='$_'" }) -join " OR "
+        $ServiceFilter = "State='Stopped' and ($ServiceFilter)"
+    }
 
-        forEach ($server in $DClist) {
-            forEach ($service in $collection) {
-                try {
-                    $s = Get-CimInstance Win32_Service -filter "name='$service'" -Computername $server -ErrorAction Stop
-                    $s
-                }
-                catch {
-                    Out-Null
-                }
+    process {
+        try {
+            $services = Get-CimInstance Win32_Service -filter $ServiceFilter -Computername $DClist -ErrorAction Stop
+        }
+        catch {
+            Out-Null
+        }
 
-                if ($s.State -eq "Stopped") {
-                    $Subject = "Windows Service: $($s.Displayname), is stopped on $server "
-                    $EmailBody = @"
-                                Service named <font color=Red><b>$($s.Displayname)</b></font> is stopped!
-                                Time of Event: <font color=Red><b>"""$((get-date))"""</b></font><br/>
-                                <br/>
-                                THIS EMAIL WAS AUTO-GENERATED. PLEASE DO NOT REPLY TO THIS EMAIL.
+        foreach ($service in $services) {
+            $Subject = "Windows Service: $($service.Displayname), is stopped on $service.PSComputerName "
+                $EmailBody = @"
+                            Service named <font color=Red><b>$($service.Displayname)</b></font> is stopped!
+                            Time of Event: <font color=Red><b>"""$((get-date))"""</b></font><br/>
+                            <br/>
+                            THIS EMAIL WAS AUTO-GENERATED. PLEASE DO NOT REPLY TO THIS EMAIL.
 "@
-                    $mailParams = @{
-                        To         = $Configuration.MailTo
-                        From       = $Configuration.MailFrom
-                        SmtpServer = $Configuration.SmtpServer
-                        Subject    = $Subject
-                        Body       = $EmailBody
-                        BodyAsHtml = $true
-                    }
-                    Send-MailMessage @mailParams
-                } #End If
-            } #Service Foreach
-        } #DCList Foreach
+                $mailParams = @{
+                    To         = $Configuration.MailTo
+                    From       = $Configuration.MailFrom
+                    SmtpServer = $Configuration.SmtpServer
+                    Subject    = $Subject
+                    Body       = $EmailBody
+                    BodyAsHtml = $true
+                }
+                Send-MailMessage @mailParams
+        }
     } #Process
 } #function
